2edeb8391934f7c54fb0020c259fa2f7f6d84a23
liuchuangao <liuchuangao@tenda.cn>
2020-11-23 17:58:25 +0800

    [新增][数据结构——栈]

---
+++ 数据结构/栈/Stack.js
@@ -0,0 +1,24 @@
+class Stack {
+    constructor(props) {
+        this.items = []
+    }
+    push(element){
+        this.items.push(element)
+    }
+    pop(){
+        return this.items.pop()
+    }
+    peek(){
+        return this.items[this.items.length-1]
+    }
+    isEmpty(){
+        return this.items.length === 0
+    }
+    clear(){
+        this.items = []
+    }
+    size(){
+        return this.items.length
+    }
+}
+module.exports = Stack
---
+++ 数据结构/栈/进制转换.js
@@ -0,0 +1,5 @@
+const Stack = require('./Stack')
+
+let stack = new Stack();
+stack.push('a')
+console.log(stack.size())
